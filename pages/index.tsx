f6d7321f1fbf849c6fc84897075df45b6af28e1e
Bennett Dams <bennett.dams@werum.com>
2022-03-28 13:48:36 +0200

    Add a multi-line comment to start of the render function

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
+    /**
+     * Some comment.
+     */
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
